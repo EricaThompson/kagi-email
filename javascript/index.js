2d2820a05b1f99070662e2b3e4c2914004637fdf
EricaThompson <EricaThompson.NC@gmail.com>
2025-04-26 14:48:40 -0400

    refactor to table

--- javascript/index.js
+++ javascript/index.js
@@ -12,16 +12,41 @@ const emails = [
 
 // const from = document.getElementById("from")
 
-emails.map(([sender, subject, body]) => {
-    const li = document.createElement("li");
-    li.innerHTML = `<strong>${sender}</strong> `;
-    from.appendChild(li);
-  });
+emails.map(([sender, subject, body])=> {
+  const eachEmail = document.createElement("tbody");
+  eachEmail.classList.add("each-email")
+  eachEmail.innerHTML = `
+                <tr class="raise">
+                  <td class="sender">
+                    ${sender}
+                  </td>
+                  <td class="subject" >
+                    ${subject} - 
+                  </td>
+                  <td class="message">${body}</td>
+                </tr>
+            `
+
+  table.appendChild(eachEmail);
+})
+
+
+// emails.map(([sender, subject, body]) => {
+//     const li = document.createElement("li");
+//     li.innerHTML = `<strong>${sender}</strong> `;
+//     from.appendChild(li);
+//   });
 
 // const message = document.getElementById("message")
 
-emails.map(([from, subject, body]) => {
-    const li = document.createElement("li");
-    li.innerHTML = `<strong>${subject}</strong> -- <small>${body}</small>`;
-    message.appendChild(li);
-  });
+// emails.map(([sender, subject, body]) => {
+//     const li = document.createElement("li");
+//     li.innerHTML = `<strong>${subject}</strong> -- <small>${body}</small>`;
+//     message.appendChild(li);
+// });
+
+emails.map(() => {
+  const separator = document.createElement("div");
+  separator.innerHTML = `<div></div>`
+  line.appendChild(separator)
+})
