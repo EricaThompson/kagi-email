03e97f63dccc6c0e53630eb21a4a425749085055
EricaThompson <EricaThompson.NC@gmail.com>
2025-04-29 00:24:35 -0400

    move dummy emails to backend

---
+++ backend/data/emails.js
@@ -0,0 +1,28 @@
+const emails = [
+    ["tina", "getting into it", `click this email to expand 
+      
+      you can then see the rest of the contents here and below
+      
+      if there is more content`, 1],
+    ["nina - the one and only", "today", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 2], 
+    ["celine", "if only", `final email, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`, 3],
+    ["tina", "season's greetings", `this is an email, 
+      
+      Lorem ipsum dolor sit amet, 
+      
+      consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+      
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+      Duis aute irure dolor in reprehenderit in voluptate 
+
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum`, 4],
+    ["nina", "goodnight", "another email", 5], 
+    ["celine", "weather update", "final email", 6],
+    ["tina", "time for sunset", "this is an email", 7],
+    ["nina", "leaves are here", "another email", 8], 
+    ["celine", "spring's on the way", "final email", 9]
+];
+
+export default emails
