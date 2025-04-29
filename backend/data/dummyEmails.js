eca67c9636b92f9cbc5253f68154f1f0671f5327
EricaThompson <EricaThompson.NC@gmail.com>
2025-04-29 03:00:05 -0400

    add server dummy emails

---
+++ backend/data/dummyEmails.js
@@ -0,0 +1,122 @@
+const emails = [
+    {
+      sender: "tina",
+      date: "2025-01-13",
+      subject: "getting into it",
+      body: `click this email to expand 
+        
+        you can then see the rest of the contents here and below
+        
+        if there is more content`,
+      id: 1
+    },
+    {
+      sender: "nina - the one and only",
+      date: "2025-03-05",
+      subject: "today",
+      body: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+      id: 2
+    },
+    {
+      sender: "celine",
+      date: "2025-05-02",
+      subject: "if only",
+      body: `final email, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+      id: 3
+    },
+    {
+      sender: "tina",
+      date: "2025-06-30",
+      subject: "season's greetings",
+      body: `this is an email, 
+        
+        Lorem ipsum dolor sit amet, 
+        
+        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+        Duis aute irure dolor in reprehenderit in voluptate 
+  
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum`,
+      id: 4
+    },
+    {
+      sender: "nina",
+      date: "2025-07-16",
+      subject: "goodnight",
+      body: `another email
+  Lorem ipsum dolor sit amet, 
+        
+        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+        Duis aute irure dolor in reprehenderit in voluptate 
+  
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum`,
+      id: 5
+    },
+    {
+      sender: "celine",
+      date: "2025-08-29",
+      subject: "weather update",
+      body: `final email Lorem ipsum dolor sit amet, 
+        
+        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+        Duis aute irure dolor in reprehenderit in voluptate 
+  
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum`,
+      id: 6
+    },
+    {
+      sender: "tina",
+      date: "2025-10-04",
+      subject: "time for sunset",
+      body: `this is an email
+  Lorem ipsum dolor sit amet, 
+        
+        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+        Duis aute irure dolor in reprehenderit in voluptate 
+  
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum`,
+      id: 7
+    },
+    {
+      sender: "nina",
+      date: "2025-11-22",
+      subject: "leaves are here",
+      body: `another email Lorem ipsum dolor sit amet, 
+        
+        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+        Duis aute irure dolor in reprehenderit in voluptate 
+  
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum`,
+      id: 8
+    },
+    {
+      sender: "celine",
+      date: "2025-12-10",
+      subject: "spring's on the way",
+      body: `final email Lorem ipsum dolor sit amet, 
+        
+        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+        Duis aute irure dolor in reprehenderit in voluptate 
+  
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum`,
+      id: 9
+    }
+  ];
+  
+
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+module.exports = emails;
