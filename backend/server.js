8a1fdca1fcb4c5f19b3bbc57abbdc2147e8baed4
EricaThompson <EricaThompson.NC@gmail.com>
2025-05-02 21:13:21 -0400

    add node server, remove net server

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,15 @@
-const net = require('net');
+const http = require('http');
 const port = 5197;
-const emails = require('./data/dummyEmails.js');
+const emails = require('./data/dummyEmails');
 
-const server = net.createServer((socket) => {
-  console.log('server connected');
-  
-  socket.write('* OK IMAP4rev1 service started\r\n');
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET' && req.url.includes('/emails') ){
 
-});
+    res.writeHead(200);
+    res.end(JSON.stringify(emails))
+  }
+})
 
 server.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+  console.log(`backend api @ http://localhost:${port}`);
+})
