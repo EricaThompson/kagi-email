6752ec85ea3a0bb67b3b50aada1303b78a94a0f2
EricaThompson <EricaThompson.NC@gmail.com>
2025-05-02 23:02:25 -0400

    add cors

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ const port = 5197;
 const emails = require('./data/dummyEmails');
 
 const server = http.createServer((req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+
   if (req.method === 'GET' && req.url.includes('/emails') ){
 
     res.writeHead(200);
