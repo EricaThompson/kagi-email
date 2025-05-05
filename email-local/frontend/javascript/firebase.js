12d888e1ddf5bb559c66d2c0f510b90348d6e2ad
EricaThompson <EricaThompson.NC@gmail.com>
2025-05-04 23:59:32 -0400

    add firebase

---
+++ email-local/frontend/javascript/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-analytics.js";
+import { getFirestore, collection, getDocs, query, where } from 'https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBj3640UPz59Cj8eP8yVq_U7oPQpvehR0g",
+  authDomain: "kagi-email-app.firebaseapp.com",
+  projectId: "kagi-email-app",
+  storageBucket: "kagi-email-app.firebasestorage.app",
+  messagingSenderId: "964381072526",
+  appId: "1:964381072526:web:d8d4eb9d30e728b482bddf",
+  measurementId: "G-HCT6J0BEQY"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+export async function getEmailsByFolder(folder) {
+    const folderQuery = query(collection(db, "emails"), where("folder", "==", folder))
+    const snapshot = await getDocs(folderQuery);
+    return snapshot.docs.map(doc => doc.data());
+}
