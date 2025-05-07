f35475b40d12749326b61d6c7309b8e1ffa2416c
EricaThompson <EricaThompson.NC@gmail.com>
2025-05-07 14:35:53 -0400

    reconfigure firebaseConfig object for vercel

--- backend/firebase.js
+++ backend/firebase.js
@@ -5,19 +5,19 @@ import { getFirestore, collection, getDocs, addDoc, deleteDoc, updateDoc, query,
 import firebaseConfig from './firebaseConfig.js';
 
 let config
-if (VERCEL){
-  config = {
-    FIREBASE_API_KEY,
-    FIREBASE_AUTH_DOMAIN,
-    FIREBASE_PROJECT_ID,
-    FIREBASE_STORAGE_BUCKET,
-    FIREBASE_MESSAGING_SENDER_ID,
-    FIREBASE_APP_ID,
-    FIREBASE_MEASUREMENT_ID
+if (process.env.VERCEL){
+    config = {
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.FIREBASE_APP_ID,
+      measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    }
+  } else {
+    config = firebaseConfig;
   }
-} else {
-  config = firebaseConfig;
-}
 
 
 const app = initializeApp(config);
--- index.js
+++ index.js
@@ -8,15 +8,15 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebas
 import firebaseConfig from './backend/firebaseConfig.js';
 
 let config
-if (VERCEL){
+if (process.env.VERCEL){
   config = {
-    FIREBASE_API_KEY,
-    FIREBASE_AUTH_DOMAIN,
-    FIREBASE_PROJECT_ID,
-    FIREBASE_STORAGE_BUCKET,
-    FIREBASE_MESSAGING_SENDER_ID,
-    FIREBASE_APP_ID,
-    FIREBASE_MEASUREMENT_ID
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
   }
 } else {
   config = firebaseConfig;
